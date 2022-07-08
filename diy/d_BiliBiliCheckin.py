ed77071507243de811fdeb1e63c68fc97de7e544
QiYueYi <64852147+QiYueYiya@users.noreply.github.com>
2022-07-08 17:23:13 +0800

    Update d_BiliBiliCheckin.py

--- diy/d_BiliBiliCheckin.py
+++ diy/d_BiliBiliCheckin.py
@@ -177,23 +177,26 @@ def main():
         report_ret = report_task(session=session,bili_jct=bili_jct,aid=vaid,bvid=vbvid)
         if report_ret["code"]==0:
             List.append(f"观看视频：{vtitle}")
+            print(f"观看视频：{vtitle}")
             vidoe_exp = 5
         else:
             List.append(f"观看视频：任务失败")
+            print(f"观看视频：任务失败")
             vidoe_exp = 0
-        sleep(500)
         # 开始分享
         sharevideo = share_task(session,bili_jct,vaid)
         if sharevideo["code"]==0:
             List.append(f'分享成功：{vtitle}')
+            print(f'分享成功：{vtitle}')
             share_exp=5
         elif sharevideo["code"]==71000:
             List.append(f'重复分享：{vtitle}')
+            print(f'重复分享：{vtitle}')
             share_exp=5
         else:
             List.append(f'分享任务：{vtitle}{sharevideo["message"]}')
+            print(f'分享任务：{vtitle}{sharevideo["message"]}')
             share_exp=0
-        sleep(500)
         if coin_num > 0:
             success_coin = get_coin(session)
             for videomsg in video_list[::-1]:
@@ -205,20 +208,26 @@ def main():
                 if ret["code"]==0:
                     success_coin+=1
                     List.append(f'投币成功：{title}：')
+                    print(f'投币成功：{title}：')
                 elif ret["code"]==34005:
                     List.append(f'投币失败：{title}，{ret["message"]}')
+                    print(f'投币失败：{title}，{ret["message"]}')
                 else:
                     List.append(f'投币失败：{title}，{ret["message"]}，跳过投币')
+                    print(f'投币失败：{title}，{ret["message"]}，跳过投币')
                     break
-                sleep(500)
             List.append(f"投币任务：已投币{success_coin}个")
+            print(f"投币任务：已投币{success_coin}个")
         else:
             List.append("投币任务：无需投币")
+            print("投币任务：无需投币")
         # 结束
         today_exp = success_coin*10+share_exp+vidoe_exp+5
         List.append(f"今日经验：{today_exp}")
+        print(f"今日经验：{today_exp}")
         update_data = round((next_exp-(current_exp+today_exp))/today_exp)
         List.append(f"升级还需：{update_data}天")
+        print(f"升级还需：{update_data}天")
 
 if __name__ == "__main__":
     if 'QYWX_Server' in os.environ:
@@ -246,7 +255,7 @@ if __name__ == "__main__":
         for cookie in users:
             main()
         tt = '\n'.join(List)
-        print(tt)
+        # print(tt)
         if 'QYWX_Server' in os.environ:
             push('哔哩哔哩', tt)
     else:
