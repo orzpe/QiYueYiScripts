44541d8c4d8fccba58d9445945eb387454169f1e
QiYueYi <64852147+QiYueYiya@users.noreply.github.com>
2022-07-21 20:13:11 +0800

    Update jd_cardcheck.py

--- jd_scripts/jd_cardcheck.py
+++ jd_scripts/jd_cardcheck.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 # -*- coding: utf8 -*-
 """
-# 目前仅支持单个脚本更新的情况下，如果该仓库一次更新多个脚本，则此脚本有可能出错
-# 需要安装第三方依赖：deepdiff
-
-# 要监控的GitHub仓库，例如监控的仓库为：https://github.com/opencard/scripts
-# 则填写链接中的用户名和仓库名，如下
+# 通过监控Github仓库来查看是否有新的开卡脚本
+# 如果有则自动拉库并运行开卡脚本
+# 此脚本需要安装第三方依赖：deepdiff
+# 填写要监控的GitHub仓库的用户名和仓库名
 export GitRepoHost="opencard/scripts"
 
 cron: */5 0-6 * * *
@@ -25,10 +24,21 @@ def qlcron(name):
         return jsons["data"][0]["name"],[jsons["data"][0]["_id"]]
     else:
         print(f'请求失败：{url}')
+        print("错误信息："+json.loads(rsp.text)["message"])
         return False,False
 
 def qlrun(scripts_name):
+    # 读取青龙登录token
+    with open("/ql/config/auth.json", 'rb') as json_file:
+        authjson = json.load(json_file)
+    if "token" in authjson:
+        token = authjson["token"]
+    else:
+        print("青龙Token获取失败")
+        return
     url = host+"/crons/run"
+    # 向请求头添加青龙登录Token
+    headers['Authorization']='Bearer '+token
     GitPath = GitRepo.split("/")
     RepoName,RepoID = qlcron(GitRepo)
     if not RepoName:
@@ -37,11 +47,13 @@ def qlrun(scripts_name):
     # 运行拉取仓库任务
     File = os.path.exists("/ql/scripts/"+GitPath[0]+"_"+GitPath[1]+"/"+scripts_name)
     while not File:
+        print(f"目标文件夹没有{scripts_name}文件，即将更新仓库")
         rsp = session.put(url=url,headers=headers,data=json.dumps(RepoID))
         if rsp.status_code == 200:
             print(f"运行任务：{RepoName}")
         else:
             print(f'请求失败：{url}')
+            print("错误信息："+json.loads(rsp.text)["message"])
         sleep(10)
         File = os.path.exists("/ql/scripts/"+GitPath[0]+"_"+GitPath[1]+"/"+scripts_name)
     # 运行开卡任务
@@ -54,54 +66,54 @@ def qlrun(scripts_name):
         print(f"运行任务：{TaskName}")
     else:
         print(f'请求失败：{url}')
+        print("错误信息："+json.loads(rsp.text)["message"])
         return
 
 def main():
-    # 请求Github获取目录树
+    # 请求Github仓库获取目录树
     rsp = session.get(url=api,headers=headers)
     if rsp.status_code != 200:
         print(f'请求失败：{api}')
         return
-    # 只保存目录中的文件名信息
+    # 只保存目录树中的开卡脚本的文件名信息
     tree = []
     for x in json.loads(rsp.text)["tree"]:
-        tree.append(x["path"])
-    # 查看是否有目录树json
+        if "opencard" in x["path"]:
+            tree.append(x["path"])
+    # 查看是否有tree.json文件
     if not os.path.exists("./tree.json"):
         with open("./tree.json","w") as f:
             json.dump(tree,f)
-        print("当前没有已保存的目录树，将保存当前目录树")
-    # 读取上一次保存的目录树并与当前目录树进行对比
+        print("没有找到tree.json文件！即将保存数据为tree.json文件")
+    # 读取上一次保存的tree.json并与当前tree进行对比
     with open("./tree.json", 'rb') as json_file:
         tree_json = json.load(json_file)
     diff = deepdiff.DeepDiff(tree_json, tree)
-    # 判断是否有新增文件
+    # 判断是否有新增开卡脚本
     if "values_changed" in diff:
         for x in diff["values_changed"]:
             scripts_name = diff["values_changed"][x]["new_value"]
-            # 如果新增文件名包含opencard
-            if "opencard" in scripts_name:
-                print(f"新增开卡：{scripts_name}")
-                # 拉库运行开卡脚本
-                headers['Authorization']='Bearer '+token
-                qlrun(scripts_name)
-            else:
-                print("仓库有更新，但没有新增开卡脚本")
+            print(f"新增开卡脚本：{scripts_name}")
+            # 拉库运行开卡脚本
+            qlrun(scripts_name)
+            break
+    elif "iterable_item_added" in diff:
+        for x in diff["iterable_item_added"]:
+            scripts_name = diff["iterable_item_added"][x]
+            print(f"新增开卡脚本：{scripts_name}")
+            # 拉库运行开卡脚本
+            qlrun(scripts_name)
             break
     else:
         print("没有新增开卡脚本")
-    # 保存当前目录树
     with open("./tree.json","w") as f:
-        json.dump(tree,f)
         print("保存当前目录树")
+        json.dump(tree,f)
 
 if 'GitRepoHost' in os.environ:
     GitRepo = os.environ['GitRepoHost']
     api = f'https://api.github.com/repos/{GitRepo}/git/trees/main'
     host = 'http://127.0.0.1:5700/api'
-    # 读取青龙登录token
-    with open("/ql/config/auth.json", 'rb') as json_file:
-        token = json.load(json_file)["token"]
     headers = {
         'accept': 'application/json',
         "Content-Type": "application/json;charset=UTF-8"
