6c3783161701dd392b5fa57cd7b0eab883b0dff8
QiYueYi <64852147+QiYueYiya@users.noreply.github.com>
2022-07-22 08:03:48 +0800

    Update jd_cardcheck.py

--- jd_scripts/jd_cardcheck.py
+++ jd_scripts/jd_cardcheck.py
@@ -14,17 +14,42 @@ new Env('开卡更新检测')
 from time import sleep
 import requests,deepdiff,json,os
 
+# 企业微信推送
+def push(title,content):
+    # 获得access_token
+    url = 'https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=' + corpid + '&corpsecret=' + corpsecret
+    re = requests.get(url).json()
+    access_token = re['access_token']
+    url1 = "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token="+ access_token
+    data = {
+       "touser" : touser,
+       "msgtype" : "news",
+       "agentid" : agentid,
+       "news" : {
+       "articles" : [{
+               "title" : title,
+               "description" : content,
+            }]
+        }
+    }
+    # 字符串格式
+    re1 = requests.post(url=url1, data=json.dumps(data)).json()
+    if re1['errcode'] == 0:
+        print("推送成功")
+    else:
+        print("推送失败")
+
 # 获取脚本ID
 def qlcron(name):
     url = host+"/crons?searchValue="+name
     rsp = session.get(url=url, headers=headers)
     jsons = json.loads(rsp.text)
     if rsp.status_code == 200:
-        print("获取任务ID成功："+jsons["data"][0]["name"])
+        List.append("获取任务ID成功："+jsons["data"][0]["name"])
         return jsons["data"][0]["name"],[jsons["data"][0]["_id"]]
     else:
-        print(f'请求失败：{url}')
-        print("错误信息："+json.loads(rsp.text)["message"])
+        List.append(f'请求失败：{url}')
+        List.append("错误信息："+json.loads(rsp.text)["message"])
         return False,False
 
 def qlrun(scripts_name):
@@ -34,7 +59,7 @@ def qlrun(scripts_name):
     if "token" in authjson:
         token = authjson["token"]
     else:
-        print("青龙Token获取失败")
+        List.append("青龙Token获取失败")
         return
     url = host+"/crons/run"
     # 向请求头添加青龙登录Token
@@ -42,40 +67,41 @@ def qlrun(scripts_name):
     # 获取仓库任务ID
     RepoName,RepoID = qlcron(GitRepo)
     if not RepoName:
-        print(f"获取任务ID失败：{GitRepo}")
+        List.append(f"获取任务ID失败：{GitRepo}")
         return
     # 运行拉取仓库任务
     GitPath = GitRepo.split("/")
     File = os.path.exists("/ql/scripts/"+GitPath[0]+"_"+GitPath[1]+"/"+scripts_name)
     while not File:
-        print(f"目标文件夹没有{scripts_name}文件，即将更新仓库")
+        List.append(f"没有找到{scripts_name}文件，即将更新仓库")
         rsp = session.put(url=url,headers=headers,data=json.dumps(RepoID))
         if rsp.status_code == 200:
-            print(f"运行任务：{RepoName}")
+            List.append(f"运行任务：{RepoName}")
         else:
-            print(f'请求失败：{url}')
-            print("错误信息："+json.loads(rsp.text)["message"])
+            List.append(f'请求失败：{url}')
+            List.append("错误信息："+json.loads(rsp.text)["message"])
+            return
         sleep(10)
         File = os.path.exists("/ql/scripts/"+GitPath[0]+"_"+GitPath[1]+"/"+scripts_name)
     # 获取开卡任务ID
     TaskName,TaskID = qlcron(scripts_name)
     if not TaskName:
-        print(f"获取任务ID失败：{scripts_name}")
+        List.append(f"获取任务ID失败：{scripts_name}")
         return
     # 运行开卡任务
     rsp = session.put(url=url,headers=headers,data=json.dumps(TaskID))
     if rsp.status_code == 200:
-        print(f"运行任务：{TaskName}")
+        List.append(f"运行任务：{TaskName}")
     else:
-        print(f'请求失败：{url}')
-        print("错误信息："+json.loads(rsp.text)["message"])
+        List.append(f'请求失败：{url}')
+        List.append("错误信息："+json.loads(rsp.text)["message"])
         return
 
 def main():
     # 请求Github仓库获取目录树
     rsp = session.get(url=api,headers=headers)
     if rsp.status_code != 200:
-        print(f'请求失败：{api}')
+        List.append(f'请求失败：{api}')
         return
     # 只保存目录树中的开卡脚本的文件名信息
     tree = []
@@ -86,35 +112,48 @@ def main():
     if not os.path.exists("./tree.json"):
         with open("./tree.json","w") as f:
             json.dump(tree,f)
-        print("没有找到tree.json文件！即将保存数据为tree.json文件")
+        List.append("没有找到tree.json文件！即将保存数据为tree.json文件")
     # 读取上一次保存的tree.json并与当前tree进行对比
     with open("./tree.json", 'rb') as json_file:
         tree_json = json.load(json_file)
     diff = deepdiff.DeepDiff(tree_json, tree)
+    state=True
     # 判断是否有新增开卡脚本
     if "values_changed" in diff:
         for x in diff["values_changed"]:
             scripts_name = diff["values_changed"][x]["new_value"]
-            print(f"新增开卡脚本：{scripts_name}")
+            List.append(f"新增开卡脚本：{scripts_name}")
             # 拉库运行开卡脚本
             qlrun(scripts_name)
             break
     elif "iterable_item_added" in diff:
         for x in diff["iterable_item_added"]:
             scripts_name = diff["iterable_item_added"][x]
-            print(f"新增开卡脚本：{scripts_name}")
+            List.append(f"新增开卡脚本：{scripts_name}")
             # 拉库运行开卡脚本
             qlrun(scripts_name)
             break
     else:
-        print("没有新增开卡脚本")
+        List.append("没有新增开卡脚本")
+        state=False
     with open("./tree.json","w") as f:
-        print("保存数据到tree.json文件")
+        List.append("保存数据到tree.json文件")
         json.dump(tree,f)
+    tt = '\n'.join(List)
+    print(tt)
+    if state:
+        push('监控开卡', tt)
 
+if 'QYWX_Server' in os.environ:
+    qywx = os.environ['QYWX_Server'].split(',')
+    corpid = qywx[0]
+    corpsecret = qywx[1]
+    touser = qywx[2]
+    agentid = qywx[3]
 if 'GitRepoHost' in os.environ:
+    List = []
     GitRepo = os.environ['GitRepoHost']
-    print(f"监控仓库：https://github.com/{GitRepo}")
+    List.append(f"监控仓库：https://github.com/{GitRepo}")
     api = f'https://api.github.com/repos/{GitRepo}/git/trees/main'
     host = 'http://127.0.0.1:5700/api'
     headers = {
